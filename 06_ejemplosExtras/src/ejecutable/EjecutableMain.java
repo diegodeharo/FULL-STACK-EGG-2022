4cb676a01f32444e67c3b21483e0944b27f5c0e6
DiegodeHaro <diegodeharo2011@gmail.com>
2022-08-24 20:24:39 -0300

    mi tercer commit

---
+++ 06_ejemplosExtras/src/ejecutable/EjecutableMain.java
@@ -0,0 +1,37 @@
+package ejecutable;
+
+import password.Password;
+
+public class EjecutableMain {
+
+    public static void main(String[] args) {
+
+        Password[] array = new Password[2];
+
+        for (int i = 0; i < 2; i++) {
+
+            Password password = new Password();
+
+            password.generarPassword();
+
+            array[i] = password;
+
+        }
+
+        System.out.println("/////////////////////////////////////////////////////////////////////////////");
+        System.out.println("/////////////////////////////////////////////////////////////////////////////");
+        System.out.println("/////////////////////////////////////////////////////////////////////////////");
+
+        for (Password i : array) {
+
+            System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+            System.out.println(i.getUsuario() + " Su Contraseña-> " + i.getContrasenia() + " es fuerte-> " + i.esFuerte());
+            System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+
+        }
+
+    }
+
+}
+
+
