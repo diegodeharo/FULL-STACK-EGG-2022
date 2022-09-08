7c33cd4e421f6e85c7af8d61f954523c96441cc5
DiegodeHaro <diegodeharo2011@gmail.com>
2022-09-08 11:56:55 -0300

    Octavo  Commmit Actualizado
    de a poco se van subiendo los ejemplos de colecciones

---
+++ 05_Guia_8_colecciones/01_colecciones_1/Colecciones_Java/src/principal/ColeccionesPrincipal.java
@@ -0,0 +1,39 @@
+package principal;
+
+public class ColeccionesPrincipal {
+
+    public static void main(String[] args) {
+        
+
+        //********************* DEFINICIONES **************************
+        
+        // trabajaremos con colecciones-> representa un grupo de objetos
+        // conocidos como Elementos, la única restricción es que no 
+        // crearemos objetos de tipos primitivos, sino sus equivalentes
+        // en vez de int usamos Integer
+        
+        /*
+         * Para tener muy en cuenta, las colecciones, crecen de manera dinámica
+         * (es decir de manera que se vayan agregando mas objetos) y no como los
+         * arreglos comunes, donde ponemos un indice de inicio y fin
+         */
+        
+        /*
+         * Vamos a trabajar en el Java Collections Framework del paquete
+         * Java.util -> Que es una ARQUITECTURA compuesta de Intefaces y Clases.
+         */
+        
+        /**
+         * FRAMERWORK-> posee un CONJUSTO ESTANDARIZADOS de Conceptos,
+         * Prácticas, Criterios y Herramientas para resolver un problema en
+         * particular o de indole similar.
+         */
+        
+        
+        
+        
+        
+        
+    }
+
+}
